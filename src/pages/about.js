5cb8a2ab7c197c5e314c1bbce687482359268fef
henry-dv <inbox@jhlehmann.de>
2022-09-28 23:23:54 +0200

    created some pages & modules

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>I AM A BEEEEEEEEEEEEEEEEEEE!!!!!! 🐝🐝🐝</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
